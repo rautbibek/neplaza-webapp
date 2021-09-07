997d9c18420ba23c8ebe9fa5ab7b78b9edde843a
Bibek Raut <rautbibek47@gmail.com>
2021-09-07 23:01:59 +0545

    new changes

--- resources/js/components/home/front/pages/category.vue
+++ resources/js/components/home/front/pages/category.vue
@@ -2,50 +2,53 @@
     <div>
         <nav aria-label="breadcrumb ">
             <ol class="breadcrumb ">
-                <li class="breadcrumb-item pull-right"><router-link :to="`/`">Home</router-link></li>
-                <li class="breadcrumb-item active" aria-current="page">{{category.name}}</li>
+                <li class="breadcrumb-item pull-right">
+                    <router-link :to="`/`">Home</router-link>
+                </li>
+                <li class="breadcrumb-item active" aria-current="page">
+                    {{ category.name }}
+                </li>
             </ol>
         </nav>
         <div>
-        <v-overlay :value="overlay">
-            <v-progress-circular indeterminate size="64"></v-progress-circular>
-        </v-overlay>
-        <!-- mobile view subcategoryt -->
-            <v-navigation-drawer
-                v-model="subcategory"
-                app
-                bottom
-                temporary
-                >
-                <v-list
-                    nav
-                    dense
-                  >
-                  <v-subheader>Choose your subcategory</v-subheader>
+            <v-overlay :value="overlay">
+                <v-progress-circular
+                    indeterminate
+                    size="64"
+                ></v-progress-circular>
+            </v-overlay>
+            <!-- mobile view subcategoryt -->
+            <v-navigation-drawer v-model="subcategory" app bottom temporary>
+                <v-list nav dense>
+                    <v-subheader>Choose your subcategory</v-subheader>
                     <v-list-item-group
-
-                    active-class="deep-purple--text text--accent-4"
+                        active-class="deep-purple--text text--accent-4"
                     >
-                    <v-list-item
-                        v-for="(scat, i) in category.scategory"
-                        :key="i"
-                        router :to="`/category/${category.id}/${scat.slug}`"
+                        <v-list-item
+                            v-for="(scat, i) in category.scategory"
+                            :key="i"
+                            router
+                            :to="`/category/${category.id}/${scat.slug}`"
                         >
-                        <v-list-item-icon>
-                            <v-icon >keyboard_arrow_right</v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-content>
-                            <v-list-item-title v-text="scat.name"></v-list-item-title>
-                        </v-list-item-content>
+                            <v-list-item-icon>
+                                <v-icon>keyboard_arrow_right</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-content>
+                                <v-list-item-title
+                                    v-text="scat.name"
+                                ></v-list-item-title>
+                            </v-list-item-content>
                         </v-list-item>
                     </v-list-item-group>
                 </v-list>
             </v-navigation-drawer>
-        <!-- end of mobile view subcategory -->
-        <div class="py-5" v-if="count>0">
-
-            <v-container>
-                <v-btn class="mb-3 d-flex d-sm-none" absolute  @click.stop="subcategory = !subcategory"
+            <!-- end of mobile view subcategory -->
+            <div class="py-5" v-if="count > 0">
+                <v-container>
+                    <v-btn
+                        class="mb-3 d-flex d-sm-none"
+                        absolute
+                        @click.stop="subcategory = !subcategory"
                         small
                         fab
                         bottom
@@ -56,108 +59,117 @@
                     >
                         <v-icon>mdi-reorder-horizontal</v-icon>
                     </v-btn>
-                <v-layout row wrap class="px-1">
-
-                <v-flex xs6 sm6 md4 lg2 xl2 v-for="(ads,index) in all_ads" :key="index" >
-                    <card-lazy :ads="ads"></card-lazy>
-                </v-flex>
-
-                </v-layout>
-                <div class="text-center mt-5" v-if="nextUrl">
-
-                <v-btn :loading="loading"  outlined  color="#2F3B59" @click.prevent="more(nextUrl)">
-                    Load More
-                    <template v-slot:loader>
-                        <span>Loading...</span>
-                    </template>
-                    <v-icon right>cached</v-icon>
-                </v-btn>
+                    <v-layout row wrap class="px-1">
+                        <v-flex
+                            xs6
+                            sm6
+                            md4
+                            lg3
+                            xl2
+                            v-for="(ads, index) in all_ads"
+                            :key="index"
+                        >
+                            <card-lazy :ads="ads"></card-lazy>
+                        </v-flex>
+                    </v-layout>
+                    <div class="text-center mt-5" v-if="nextUrl">
+                        <v-btn
+                            :loading="loading"
+                            outlined
+                            color="#2F3B59"
+                            @click.prevent="more(nextUrl)"
+                        >
+                            Load More
+                            <template v-slot:loader>
+                                <span>Loading...</span>
+                            </template>
+                            <v-icon right>cached</v-icon>
+                        </v-btn>
+                    </div>
+                </v-container>
             </div>
-
-            </v-container>
-        </div>
-        <div v-else>
-            <empty></empty>
-        </div>
+            <div v-else>
+                <empty></empty>
+            </div>
         </div>
     </div>
 </template>
 <script>
-
 export default {
-    data(){
-      return{
-        overlay:true,
-        loading:false,
-        count:1,
-        all_ads:[],
-        category:{},
-        subcategory:false,
-        nextUrl : null,
-        items: [
-        { text: 'Real-Time', icon: 'mdi-clock' },
-        { text: 'Audience', icon: 'mdi-account' },
-        { text: 'Conversions', icon: 'mdi-flag' },
-      ],
-      }
+    data() {
+        return {
+            overlay: true,
+            loading: false,
+            count: 1,
+            all_ads: [],
+            category: {},
+            subcategory: false,
+            nextUrl: null,
+            items: [
+                { text: "Real-Time", icon: "mdi-clock" },
+                { text: "Audience", icon: "mdi-account" },
+                { text: "Conversions", icon: "mdi-flag" }
+            ]
+        };
     },
-    methods:{
+    methods: {
+        favorite() {
+            if (this.$loggedIn) {
+                this.color = "red";
 
-      favorite(){
-        if(this.$loggedIn){
-            this.color= 'red';
-
-            this.background= 'white';
-        }else{
-            EventBus.$emit('changeDialog', true);
-        }
-      },
-
-      fetch(url){
-          this.overlay=true;
-          axios.get(url)
-               .then(({data}) =>{
-                     this.all_ads = data[0].data;
-                     this.nextUrl = data[0].next_page_url
-                     this.category = data[1];
-                     this.count = this.all_ads.length;
-                     this.overlay= false;
-            }).catch((error) => {
-            if (error.response.status === 404) {
-              window.location.href = '/pageNotFound'
+                this.background = "white";
+            } else {
+                EventBus.$emit("changeDialog", true);
             }
-          });
-      },
-      more(nextUrl){
-        this.loading=true;
-        axios.get(nextUrl)
-             .then(({data}) =>{
+        },
+
+        fetch(url) {
+            this.overlay = true;
+            axios
+                .get(url)
+                .then(({ data }) => {
+                    this.all_ads = data[0].data;
+                    this.nextUrl = data[0].next_page_url;
+                    this.category = data[1];
+                    this.count = this.all_ads.length;
+                    this.overlay = false;
+                })
+                .catch(error => {
+                    if (error.response.status === 404) {
+                        window.location.href = "/pageNotFound";
+                    }
+                });
+        },
+        more(nextUrl) {
+            this.loading = true;
+            axios.get(nextUrl).then(({ data }) => {
                 this.all_ads.push(...data[0].data);
-                this.nextUrl = data[0].next_page_url
+                this.nextUrl = data[0].next_page_url;
                 this.loading = false;
-            })
-      }
+            });
+        }
     },
-    created(){
+    created() {
         //alert(this.$route.params.slug)
         this.fetch(`/front/category/product/${this.$route.params.slug}`);
     },
     metaInfo() {
-    return {
-      meta: [
-        { name: "title", content: this.category.name },
-        { property: "og:title", content: this.category.name },
-        { property: "og:site_name", content: "Bikribazzar" },
-        { property: "og:type", content: "website" },
-        { name: "robots", content: "index,follow" },
-      ],
-    };
-  },
-    watch:{
-      $route(to,from){
-        return this.fetch(`/front/category/product/${this.$route.params.slug}`);
-      }
+        return {
+            meta: [
+                { name: "title", content: this.category.name },
+                { property: "og:title", content: this.category.name },
+                { property: "og:site_name", content: "Bikribazzar" },
+                { property: "og:type", content: "website" },
+                { name: "robots", content: "index,follow" }
+            ]
+        };
+    },
+    watch: {
+        $route(to, from) {
+            return this.fetch(
+                `/front/category/product/${this.$route.params.slug}`
+            );
+        }
     }
-
-}
+};
 </script>
--- resources/js/components/home/front/pages/product.vue
+++ resources/js/components/home/front/pages/product.vue
@@ -2,8 +2,12 @@
     <div>
         <nav aria-label="breadcrumb ">
             <ol class="breadcrumb ">
-                <li class="breadcrumb-item pull-right"><router-link :to="`/`">Home</router-link></li>
-                <li class="breadcrumb-item active" aria-current="page">All Ads</li>
+                <li class="breadcrumb-item pull-right">
+                    <router-link :to="`/`">Home</router-link>
+                </li>
+                <li class="breadcrumb-item active" aria-current="page">
+                    All Ads
+                </li>
             </ol>
         </nav>
         <v-overlay :value="overlay">
@@ -15,120 +19,131 @@
                 <h3 class="text-center">All Ads</h3>
             </div>
             <v-container class="cover-container create-ad">
-            <v-expansion-panels>
-                <v-expansion-panel v-for="(category, index) in categories" :key="index">
-                <v-expansion-panel-header>
-                    <v-icon style="display:block;" :to="`/category/${category.slug}`"
-                    left
-                    medium
-                >{{category.icons}}</v-icon>{{ category.name }}
-                </v-expansion-panel-header>
-                <v-expansion-panel-content class="panel-content">
-                    <ul class="list-group">
-                    <router-link
-                        :to="`/category/${category.id}/${cat.slug}`"
-                        class="list-group-item list-group-item-action"
-                        v-for="(cat, index) in category.scategory"
+                <v-expansion-panels>
+                    <v-expansion-panel
+                        v-for="(category, index) in categories"
                         :key="index"
                     >
-                        {{ cat.name }}
-                    </router-link>
-                    </ul>
-                </v-expansion-panel-content>
-                </v-expansion-panel>
-            </v-expansion-panels>
+                        <v-expansion-panel-header>
+                            <v-icon
+                                style="display:block;"
+                                :to="`/category/${category.slug}`"
+                                left
+                                medium
+                                >{{ category.icons }}</v-icon
+                            >{{ category.name }}
+                        </v-expansion-panel-header>
+                        <v-expansion-panel-content class="panel-content">
+                            <ul class="list-group">
+                                <router-link
+                                    :to="`/category/${category.id}/${cat.slug}`"
+                                    class="list-group-item list-group-item-action"
+                                    v-for="(cat, index) in category.scategory"
+                                    :key="index"
+                                >
+                                    {{ cat.name }}
+                                </router-link>
+                            </ul>
+                        </v-expansion-panel-content>
+                    </v-expansion-panel>
+                </v-expansion-panels>
             </v-container>
         </div>
         <!-- end of extra menu -->
-      <div class="">
-        <v-container>
-            <v-layout row wrap class="px-1">
-
-                <v-flex xs6 sm6 md4 lg2 xl2 v-for="(ads,index) in all_ads" :key="index" >
-                    <card-lazy :ads="ads"></card-lazy>
-                </v-flex>
-
-            </v-layout>
-            <div class="text-center ma-5" v-if="nextUrl">
-
-            <v-btn :loading="loading" outlined tile color="#2F3B59" class="" @click.prevent="more(nextUrl)">
-
-                Load More
-                <template v-slot:loader>
-                    <span>Loading...</span>
-                </template>
-                <v-icon right>cached</v-icon>
-            </v-btn>
-            </div>
-
-        </v-container>
-      </div>
+        <div class="">
+            <v-container>
+                <v-layout row wrap class="px-1">
+                    <v-flex
+                        xs6
+                        sm6
+                        md4
+                        lg3
+                        xl2
+                        v-for="(ads, index) in all_ads"
+                        :key="index"
+                    >
+                        <card-lazy :ads="ads"></card-lazy>
+                    </v-flex>
+                </v-layout>
+                <div class="text-center ma-5" v-if="nextUrl">
+                    <v-btn
+                        :loading="loading"
+                        outlined
+                        tile
+                        color="#2F3B59"
+                        class=""
+                        @click.prevent="more(nextUrl)"
+                    >
+                        Load More
+                        <template v-slot:loader>
+                            <span>Loading...</span>
+                        </template>
+                        <v-icon right>cached</v-icon>
+                    </v-btn>
+                </div>
+            </v-container>
+        </div>
     </div>
 </template>
 <script>
-
 export default {
-    data(){
-      return{
-        isActive: false,
-        loading:false,
-        overlay:false,
-        all_ads:[],
-        categories:{},
-        color:'white',
-        background:'#270f0ea1',
-        nextUrl : null,
-      }
+    data() {
+        return {
+            isActive: false,
+            loading: false,
+            overlay: false,
+            all_ads: [],
+            categories: {},
+            color: "white",
+            background: "#270f0ea1",
+            nextUrl: null
+        };
     },
-    methods:{
-      favorite(){
-        if(this.$loggedIn){
-            this.color= 'red';
-            this.background= 'white';
-        }else{
-            EventBus.$emit('changeDialog', true);
-        }
-      },
+    methods: {
+        favorite() {
+            if (this.$loggedIn) {
+                this.color = "red";
+                this.background = "white";
+            } else {
+                EventBus.$emit("changeDialog", true);
+            }
+        },
 
-      fetch(url){
-          this.overlay= true
-          this.loading=true;
-          axios.get(url)
-               .then(({data}) =>{
-                     this.all_ads.push(...data.data);
-                     this.nextUrl = data.next_page_url
-                     this.loading = false;
-                     this.overlay = false;
-                 })
-      },
-      more(nextUrl){
-        this.loading=true;
-        axios.get(nextUrl)
-             .then(({data}) =>{
+        fetch(url) {
+            this.overlay = true;
+            this.loading = true;
+            axios.get(url).then(({ data }) => {
                 this.all_ads.push(...data.data);
-                this.nextUrl = data.next_page_url
+                this.nextUrl = data.next_page_url;
                 this.loading = false;
-            })
-      }
+                this.overlay = false;
+            });
+        },
+        more(nextUrl) {
+            this.loading = true;
+            axios.get(nextUrl).then(({ data }) => {
+                this.all_ads.push(...data.data);
+                this.nextUrl = data.next_page_url;
+                this.loading = false;
+            });
+        }
     },
 
-    created(){
+    created() {
         this.fetch(`front/all/product`);
         axios.get(this.subcategory_url).then(response => {
             this.categories = response.data;
         });
     },
     computed: {
-    subcategory_url() {
-      return `/menu/subCategory`;
+        subcategory_url() {
+            return `/menu/subCategory`;
+        }
     }
-  }
-
-
-}
+};
 </script>
 <style scoped>
-    .v-expansion-panel-header > *:not(.v-expansion-panel-header__icon) {
+.v-expansion-panel-header > *:not(.v-expansion-panel-header__icon) {
     flex: none;
 }
 </style>
